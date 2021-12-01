3f1ff026940dd8a1f97eed9ffe20c4daf1b121d8
Bridgette Quiambao <bridgettequiambao@gmail.com>
2021-12-01 10:41:25 -0800

    starter code

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProtectedPageExample from "./pages/ProtectedPageExample";
+import SignUp from "./pages/SignUp";
+import { client } from "./util/apolloClient";
+import { AuthProvider } from "./util/auth";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            {/* Use PrivateRoute for pages that should only be accessible to a
+            user that has logged in.*/}
+            <PrivateRoute path="/protected">
+              <ProtectedPageExample />
+            </PrivateRoute>
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ server/config/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log(
+      "⛔ There was an error connecting to MongoDB. See above for details."
+    );
+    console.log("Shutting down.");
+    process.exit(1);
+  });
+
+module.exports = mongoose.connection;
