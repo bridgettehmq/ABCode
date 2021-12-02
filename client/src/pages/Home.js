7d9b0d8e22dc5f175bccd71dd607cf6a42158c04
Raghad Malallah <raghadmalallah@gmail.com>
2021-12-02 10:33:03 -0800

    adding a folder for images in the puplic and working on the home.js the src

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,29 @@
 import { useAuth } from "../util/auth";
-
+import "./home.css";
+function clickMe(){
+  alert('you clicked me!');
+}
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
-    <div>
+    <div className="container">
       {/* TODO: display logged in user's username */}
+      <img src="/images/abcode-logo.png" alt="ab-code-logo" />
       <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
-      <hr />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo cumque
-        explicabo ipsum, facilis repellendus omnis amet in accusantium quisquam
-        nam qui consectetur sunt distinctio nemo molestiae ratione. Iure,
-        aliquam debitis.
+        Invest in your child’s future with fun flash cards for the future coding
+        language.
       </p>
+      <p>
+        {" "}
+        To get started, click below to learn about HTML, CSS and JAVASCRIPT.
+      </p>
+      <div>
+      <button onClick={clickMe}>
+        button
+      </button>
     </div>
+    </div>
+   
   );
 }
