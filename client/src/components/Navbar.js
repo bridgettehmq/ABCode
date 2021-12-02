0195fad2ca28d523c47cf61728a51ce833ed2be7
Raghad Malallah <raghadmalallah@gmail.com>
2021-12-02 13:38:11 -0800

    adding a nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,64 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../util/auth";
+import { Navbar, Offcanvas, Container, NavDropdown,Form, Button, Nav, FormControl} from 'react-bootstrap';
+
 import "./Navbar.css";
 
-export default function Navbar() {
-  const { isLoggedIn, logout } = useAuth();
+export default function nav() {
+  // const { isLoggedIn, logout } = useAuth();
   return (
-    <nav className="navbar">
-      <NavLink exact to="/" className="navbar-link">
-        Home
-      </NavLink>
-      <NavLink exact to="/info" className="navbar-link">
-        Info
-      </NavLink>
-      {isLoggedIn ? (
-        <>
-          <NavLink to="/protected" className="navbar-link">
-            User
-          </NavLink>
-          <button className="navbar-link" onClick={logout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <NavLink to="/login" className="navbar-link">
-            Login
-          </NavLink>
-          <NavLink to="/signup" className="navbar-link">
-            Signup
-          </NavLink>
-        </>
-      )}
-    </nav>
+    <Navbar bg="light" expand={false}>
+  <Container fluid>
+    <Navbar.Brand href="/">ABCode</Navbar.Brand>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/info">info</Nav.Link>
+          <Nav.Link href="/">Sign Up</Nav.Link>
+          <Nav.Link href="/info">Login</Nav.Link>
+          <Nav.Link href="/info">Log Out</Nav.Link>
+          
+        </Nav>
+             </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
+    
+    // <nav className="navbar">
+    //   <NavLink exact to="/" className="navbar-link">
+    //     Home
+    //   </NavLink>
+    //   <NavLink exact to="/info" className="navbar-link">
+    //     Info
+    //   </NavLink>
+    //   {isLoggedIn ? (
+    //     <>
+    //       <NavLink to="/protected" className="navbar-link">
+    //         User
+    //       </NavLink>
+    //       <button className="navbar-link" onClick={logout}>
+    //         Logout
+    //       </button>
+    //     </>
+    //   ) : (
+    //     <>
+    //       <NavLink to="/login" className="navbar-link">
+    //         Login
+    //       </NavLink>
+    //       <NavLink to="/signup" className="navbar-link">
+    //         Signup
+    //       </NavLink>
+    //     </>
+    //   )}
+    // </nav>
   );
 }
