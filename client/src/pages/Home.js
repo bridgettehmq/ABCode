7959353a29ddedc769e292640a870d741d5ecb4e
Raghad Malallah <raghadmalallah@gmail.com>
2021-12-02 11:32:05 -0800

    adjusting the image size for the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
+
 import { useAuth } from "../util/auth";
 import "./home.css";
-function clickMe(){
-  alert('you clicked me!');
-}
+
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
     <div className="container">
       {/* TODO: display logged in user's username */}
-      <img src="/images/abcode-logo.png" alt="ab-code-logo" />
+      <img src="/images/abcode-logo.jpg" alt="ab-code-logo" />
       <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
       <p>
         Invest in your child’s future with fun flash cards for the future coding
@@ -18,11 +17,7 @@ export default function Home() {
         {" "}
         To get started, click below to learn about HTML, CSS and JAVASCRIPT.
       </p>
-      <div>
-      <button onClick={clickMe}>
-        button
-      </button>
-    </div>
+   
     </div>
    
   );
