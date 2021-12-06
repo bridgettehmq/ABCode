ff4587607d2fedb7791c2092e009818cea24d984
Zeyang Lai <decyctus@gmail.com>
2021-12-06 11:52:37 -0800

    add style to the title and fix the buttons

--- client/src/components/PageList/index.js
+++ client/src/components/PageList/index.js
@@ -12,11 +12,11 @@ const PageList = ({
     <div>
       {pages &&
         pages.map((page) => (
-          <div key={page.pageId} className="card mb-3">
-            <Card style={{ width: "24rem" }}>
+          <div key={page.pageId} className="mb-3 ">
+            <Card >
           <Card.Body>
-            <Card.Title>Page Title:{page.title}</Card.Title>
-            <Button href="./info" variant="light" className='w-100'>
+            <Card.Title style={{fontWeight:"bolder",textAlign:"center"}}>{page.title}</Card.Title>
+            <Button onClick={()=>window.location=`/pages/${page.pageId}`} variant="light" className='w-100'>
               Go to the page
             </Button>
           </Card.Body>
